b5ea2492a9ccda502858137418c3a7748c6fdee5
Moh. Walid Arkham Sani <mohwalid.jaeger@gmail.com>
2022-01-14 20:33:42 +0700

    update typing effect

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,13 +5,19 @@ import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import styles from './index.module.css';
 import HomepageFeatures from '../components/HomepageFeatures';
+import Typical from 'react-typical'
 
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
+        <h1 className="hero__title">
+          <Typical
+          steps={['PemdaKit v1', 3000, 'Pemprov Kaltim', 2000]}
+          loop={Infinity}
+          />
+        </h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
           <Link
